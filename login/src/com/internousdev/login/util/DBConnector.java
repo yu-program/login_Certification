2fcf3d786d62a33c2766bd17d087ee96be9585df
yu-program <12ym.nkmr.11ym@gamil.com>
2020-01-24 12:11:16 +0900

    1月24日

--- login/src/com/internousdev/login/util/DBConnector.java
+++ login/src/com/internousdev/login/util/DBConnector.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnector {
-	private static String driverName = "com,mysql.jdbc.Driver";
+	private static String driverName = "com.mysql.jdbc.Driver";
 	private static String url = "jdbc:mysql://localhost/logindb_yamakawa";
 
 	private static String user = "root";
